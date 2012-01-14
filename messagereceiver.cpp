5ea814b78a617679bdc76e4d6921c0ef8ee2a3c0
Sentyakova Anna <anna.sent@gmail.com>
2012-01-15 01:18:13 +0400

    ?????????

--- messagereceiver.cpp
+++ messagereceiver.cpp
@@ -33,65 +33,53 @@ void TcpMessageReceiver::processData()
     while (current->length() <= buffer.size())
     {
         current->fill(buffer);
+        if (ChatMessage *msg = dynamic_cast<ChatMessage *>(current))
         {
-            ChatMessage *msg;
-            if ((msg = dynamic_cast<ChatMessage *>(current)))
-                emit chatMessageReceive(*msg);
+            emit chatMessageReceive(*msg);
         }
+        else if (ClientConnectMessage *msg = dynamic_cast<ClientConnectMessage *>(current))
         {
-            ClientConnectMessage *msg;
-            if ((msg = dynamic_cast<ClientConnectMessage *>(current)))
-                emit clientConnectMessageReceive(*msg);
+            emit clientConnectMessageReceive(*msg);
         }
+        else if (ClientDisconnectMessage *msg = dynamic_cast<ClientDisconnectMessage *>(current))
         {
-            ClientDisconnectMessage *msg;
-            if ((msg = dynamic_cast<ClientDisconnectMessage *>(current)))
-                emit clientDisconnectMessageReceive(*msg);
+            emit clientDisconnectMessageReceive(*msg);
         }
+        else if (ConnectionAcceptedMessage *msg = dynamic_cast<ConnectionAcceptedMessage *>(current))
         {
-            ConnectionAcceptedMessage *msg;
-            if ((msg = dynamic_cast<ConnectionAcceptedMessage *>(current)))
-                emit connectionAcceptedMessageReceive(*msg);
+            emit connectionAcceptedMessageReceive(*msg);
         }
+        else if (PingMessage *msg = dynamic_cast<PingMessage *>(current))
         {
-            PingMessage *msg;
-            if ((msg = dynamic_cast<PingMessage *>(current)))
-                emit pingMessageReceive(*msg);
+            emit pingMessageReceive(*msg);
         }
+        else if (PlayersListMessage *msg = dynamic_cast<PlayersListMessage *>(current))
         {
-            PlayersListMessage *msg;
-            if ((msg = dynamic_cast<PlayersListMessage *>(current)))
-                emit playersListMessageReceive(*msg);
+            emit playersListMessageReceive(*msg);
         }
+        else if (RestartGameMessage *msg = dynamic_cast<RestartGameMessage *>(current))
         {
-            RestartGameMessage *msg;
-            if ((msg = dynamic_cast<RestartGameMessage *>(current)))
-                emit restartGameMessageReceive(*msg);
+            emit restartGameMessageReceive(*msg);
         }
+        else if (ServerReadyMessage *msg = dynamic_cast<ServerReadyMessage *>(current))
         {
-            ServerReadyMessage *msg;
-            if ((msg = dynamic_cast<ServerReadyMessage *>(current)))
-                emit serverReadyMessageReceive(*msg);
+            emit serverReadyMessageReceive(*msg);
         }
+        else if (StartGameMessage *msg = dynamic_cast<StartGameMessage *>(current))
         {
-            StartGameMessage *msg;
-            if ((msg = dynamic_cast<StartGameMessage *>(current)))
-                emit startGameMessageReceive(*msg);
+            emit startGameMessageReceive(*msg);
         }
+        else if (SurrenderMessage *msg = dynamic_cast<SurrenderMessage *>(current))
         {
-            SurrenderMessage *msg;
-            if ((msg = dynamic_cast<SurrenderMessage *>(current)))
-                emit surrenderMessageReceive(*msg);
+            emit surrenderMessageReceive(*msg);
         }
+        else if (TurnMessage *msg = dynamic_cast<TurnMessage *>(current))
         {
-            TurnMessage *msg;
-            if ((msg = dynamic_cast<TurnMessage *>(current)))
-                emit turnMessageReceive(*msg);
+            emit turnMessageReceive(*msg);
         }
+        else if (TryToConnectMessage *msg = dynamic_cast<TryToConnectMessage *>(current))
         {
-            TryToConnectMessage *msg;
-            if ((msg = dynamic_cast<TryToConnectMessage *>(current)))
-                emit tryToConnectMessageReceive(*msg);
+            emit tryToConnectMessageReceive(*msg);
         }
 
         buffer.remove(0, current->length());
@@ -138,15 +126,13 @@ void UdpMessageReceiver::processData(QByteArray &buffer, const QHostAddress &hos
     while (current->length() <= buffer.size())
     {
         current->fill(buffer);
+        if (ServerInfoMessage *msg = dynamic_cast<ServerInfoMessage *>(current))
         {
-            ServerInfoMessage *msg;
-            if ((msg = dynamic_cast<ServerInfoMessage *>(current)))
-                emit serverInfoMessageReceive(*msg, host, port);
+            emit serverInfoMessageReceive(*msg, host, port);
         }
+        else if (ServerRequestMessage *msg = dynamic_cast<ServerRequestMessage *>(current))
         {
-            ServerRequestMessage *msg;
-            if ((msg = dynamic_cast<ServerRequestMessage *>(current)))
-                emit serverRequestMessageReceive(*msg, host, port);
+            emit serverRequestMessageReceive(*msg, host, port);
         }
 
         buffer.remove(0, current->length());
