51494f31ce9af3b19362726250a1a7133258b640
Sentyakova Anna <anna.sent@gmail.com>
2012-01-29 21:00:18 +0400

    networkplayer class style refactoring

--- networkplayer.cpp
+++ networkplayer.cpp
@@ -1,13 +1,18 @@
 #include "networkplayer.h"
 
-NetworkPlayer::NetworkPlayer(QColor clr,Table* table,int wid,int hei , QGraphicsItem * parent, QGraphicsScene * scene):Player(clr,wid,hei,parent,scene),tbl(table)
+NetworkPlayer::NetworkPlayer(QColor clr, Table *table, int wid, int hei, QGraphicsItem *parent, QGraphicsScene *scene)
+    : Player(clr, wid, hei, parent, scene), tbl(table)
 {
 }
 
-void NetworkPlayer::turnComplete(QColor color,QString tile,int item,int x,int y)
+void NetworkPlayer::turnComplete(QColor color, QString tile, int item, int x, int y)
 {
-    if (color!=this->color) return;
+    if (color != this->color)
+    {
+        return;
+    }
+
     Tile til(tile.toStdString());
-    Player::turnComplete(color,tile,item,x,y);
-    tbl->Accept(x,y,til,item,true,false,color);
+    Player::turnComplete(color, tile, item, x, y);
+    tbl->Accept(x, y, til, item, true, false, color);
 }
--- networkplayer.h
+++ networkplayer.h
@@ -7,14 +7,19 @@
 class NetworkPlayer : public Player
 {
     Q_OBJECT
-public:
-    NetworkPlayer(QColor clr,Table* table,int wid=200,int hei=200 , QGraphicsItem * parent=0, QGraphicsScene * scene=0);
-public slots:
-    void turnComplete(QColor color,QString tile,int item,int x,int y);
+
+private:
+    Table *tbl;
+
 protected:
-    void deactivateAll() {active=false;update();}
-    void activateAll() {active=true;update();}
-    Table* tbl;
+    void activateAll() { active = true; update(); }
+    void deactivateAll() { active = false; update(); }
+
+public:
+    NetworkPlayer(QColor clr, Table *table, int wid = 200, int hei = 200, QGraphicsItem *parent = 0, QGraphicsScene *scene = 0);
+
+public slots:
+    void turnComplete(QColor color, QString tile, int item, int x, int y);
 };
 
 #endif
