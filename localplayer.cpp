4a81e0a90c02b9b7891c0f66f722fbdb458451ff
Sentyakova Anna <anna.sent@gmail.com>
2012-01-29 20:12:10 +0400

    localplayer class style refactoring

--- localplayer.cpp
+++ localplayer.cpp
@@ -1,5 +1,6 @@
 #include "localplayer.h"
 
-LocalPlayer::LocalPlayer(QColor clr,int wid,int hei , QGraphicsItem * parent, QGraphicsScene * scene):Player(clr,wid,hei,parent,scene)
+LocalPlayer::LocalPlayer(QColor clr, int wid, int hei, QGraphicsItem *parent, QGraphicsScene *scene)
+    : Player(clr, wid, hei, parent, scene)
 {
 }
