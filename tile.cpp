14db4b5107c61af48bb2f94468c66516dd00bcbf
Sentyakova Anna <anna.sent@gmail.com>
2012-01-29 22:42:22 +0400

    tile class style refactoring

--- tile.cpp
+++ tile.cpp
@@ -1,35 +1,40 @@
 #include "tile.h"
 #include <iostream>
+
 using namespace std;
 
 Tile::Tile(string src)
 {
     data.resize(1);
-    scor=0;
-    for(size_t i = 0; i<src.size(); ++i)
+    scor = 0;
+    for(size_t i = 0; i < src.size(); ++i)
     {
-        if(src[i]=='|')
+        if(src[i] == '|')
         {
-            data.resize(data.size()+1);
+            data.resize(data.size() + 1);
         }
         else
         {
             data.back().push_back(src[i]);
-            if (src[i]!='0') ++scor;
+            if (src[i] != '0')
+            {
+                ++scor;
+            }
         }
     }
 }
 
 void Tile::rotateTile()
 {
-    vector<vector<char> > newdata(getWidth(),vector<char>(getHeight(),'0'));
-    for(int i = 0;i<getHeight();++i)
+    vector<vector<char> > newdata(getWidth(), vector<char>(getHeight(),'0'));
+    for(int i = 0; i < getHeight(); ++i)
     {
-        for(int j = 0;j<getWidth();++j)
+        for(int j = 0; j < getWidth(); ++j)
         {
-            newdata[j][getHeight()-i-1]=data[i][j];
+            newdata[j][getHeight() - i - 1] = data[i][j];
         }
     }
+
     data.swap(newdata);
 }
 
@@ -40,47 +45,48 @@ int Tile::score() const
 
 void Tile::reflectTile()
 {
-    vector<vector<char> > newdata(getHeight(),vector<char>(getWidth(),'0'));
-    for(int i = 0;i<getHeight();++i)
+    vector<vector<char> > newdata(getHeight(), vector<char>(getWidth(), '0'));
+    for(int i = 0; i < getHeight(); ++i)
     {
-        for(int j = 0;j<getWidth();++j)
+        for(int j = 0; j < getWidth(); ++j)
         {
-            newdata[i][getWidth()-j-1]=data[i][j];
+            newdata[i][getWidth() - j - 1] = data[i][j];
         }
     }
+
     data.swap(newdata);
 }
 
 string Tile::getAsText() const
 {
     string result;
-    for(int i = 0;i<getHeight();++i)
+    for(int i = 0; i < getHeight(); ++i)
     {
-        for(int j = 0;j<getWidth();++j)
+        for(int j = 0; j < getWidth(); ++j)
         {
             result.push_back(data[i][j]);
         }
-        if (i<getHeight()-1) result.push_back('|');
+
+        if (i < getHeight() - 1)
+        {
+            result.push_back('|');
+        }
     }
+
     return result;
 }
 
-ostream& operator<<(ostream& to,const Tile& what)
+ostream &operator<<(ostream &to, const Tile &what)
 {
-    for(size_t i=0;i<what.data.size();++i)
+    for(size_t i = 0; i < what.data.size(); ++i)
     {
-        for(size_t j=0;j<what.data[i].size();++j)
+        for(size_t j = 0; j < what.data[i].size(); ++j)
         {
-            to<<what.data[i][j];
+            to << what.data[i][j];
         }
-        to<<"\n";
+
+        to << "\n";
     }
+
     return to;
 }
-
-#ifdef TEST
-int main()
-{
-    Tile tile("12|01");
-}
-#endif
--- tile.h
+++ tile.h
@@ -4,23 +4,26 @@
 #include <string>
 #include <vector>
 #include <QString>
+
 using namespace std;
 
 class Tile
 {
 public:
-    Tile(string type);
-    int getHeight() const {return data.size();}
-    int getWidth() const {return data.size()>0?data[0].size():0;}
-    void rotateTile();
-    void reflectTile();
-    int score() const;
-    vector<vector<char> > data;
-    string getAsText() const;
-    QString getAsQString() const {return QString(getAsText().c_str());}
     int scor;
+
+    Tile(string type);
+    vector<vector<char> > data;
+
+    string getAsText() const;
+    QString getAsQString() const { return QString(getAsText().c_str()); }
+    int getHeight() const { return data.size(); }
+    int getWidth() const { return data.size() > 0 ? data[0].size() : 0; }
+    int score() const;
+    void reflectTile();
+    void rotateTile();
 };
 
-ostream& operator<<(ostream&,const Tile&);
+ostream & operator<<(ostream &, const Tile &);
 
 #endif
